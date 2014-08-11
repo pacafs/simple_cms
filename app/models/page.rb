a8f504cef77d64a8718daf13baf5ee04643142da
Pedro Fernandes da Silva <paca.fs@gmail.com>
2014-08-12 00:14:22 +0100

    manny to manny

--- app/models/page.rb
+++ app/models/page.rb
@@ -2,4 +2,8 @@ class Page < ActiveRecord::Base
 	
 	belongs_to :subject # belongs_to is where the table has the FOREIGN KEY
 
+	scope :visible, lambda { where(:visible => true) }
+	scope :newest_first, lambda { order("pages.created_at DESC") }
+
+
 end
