544311b8706b1b72f7e5a5a6ebb91752f12eef2f
Pedro Fernandes da Silva <paca.fs@gmail.com>
2014-08-12 01:05:29 +0100

    many_to_many

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 class Page < ActiveRecord::Base
 	
 	belongs_to :subject # belongs_to is where the table has the FOREIGN KEY
+	has_and_belongs_to_many :editors, :class_name => "AdminUser"
 
 	scope :visible, lambda { where(:visible => true) }
 	scope :newest_first, lambda { order("pages.created_at DESC") }
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140803181605) do
+ActiveRecord::Schema.define(version: 20140811234910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,13 @@ ActiveRecord::Schema.define(version: 20140803181605) do
   end
 
   add_index "admin_users", ["username"], name: "index_admin_users_on_username", using: :btree
+
+  create_table "admin_users_pages", id: false, force: true do |t|
+    t.integer "admin_user_id"
+    t.integer "page_id"
+  end
+
+  add_index "admin_users_pages", ["admin_user_id", "page_id"], name: "index_admin_users_pages_on_admin_user_id_and_page_id", using: :btree
 
   create_table "pages", force: true do |t|
     t.integer  "subject_id"
