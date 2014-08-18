12de244bb361beac15c398fa9f81ea0e36351d88
Pedro Fernandes da Silva <paca.fs@gmail.com>
2014-08-18 02:54:14 +0100

    Final CRUD

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -29,6 +29,29 @@ class SubjectsController < ApplicationController
     @subject = Subject.find(params[:id])
   end
 
+  def update
+    @subject = Subject.find(params[:id])
+
+    @subject.update_attributes(subject_params)
+
+    if @subject.update_attributes(subject_params)
+      redirect_to(:action => 'show', :id => @subject.id)
+    else
+      render('index')
+    end
+
+  end
+
+  def delete
+    @subject = Subject.find(params[:id])
+  end
+
+  def destroy
+    @subject = Subject.find(params[:id])
+    @subject.destroy
+    redirect_to(:action => 'index')
+  end
+
   private
 
   def subject_params
